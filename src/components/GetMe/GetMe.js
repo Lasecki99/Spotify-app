516d139b81f84599c44d8b907a88c89a901a2193
Marcin Lasecki <marcin.lasecki@pearson.com>
2020-02-29 14:21:39 +0100

    add logout feature

--- src/components/GetMe/GetMe.js
+++ src/components/GetMe/GetMe.js
@@ -4,44 +4,100 @@ import { getMeFetch } from '../../store/reducers/GetMe/getMeCreator';
 import { useDispatch, useSelector } from 'react-redux'
 
 const GetMeView = styled.div`
-position: fixed;
-bottom: 20px;
-right: 50px;
-display: flex;
-align-items: center;
-border: 2px solid #ddd;
-border-radius: 20px;
-padding: 5px 10px;
-cursor: pointer;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  display: flex;
+  align-items: center;
+  border: 1px solid ${props => props.theme.alto};
+  border-radius: 20px;
+  padding: 2px 6px;
+  cursor: pointer;
+  background-color: #121212;
 
-> img {
-  width: 38px;
-  height: 38px;
+  > i {
+    color: white;
+    margin-right: 3px;
+
+    &.hide {
+      display: none;
+    }
+  }
+
+  > img {
+  width: 32px;
+  height: 32px;
   border-radius: 50px;
   margin-left: 10px;
-}
+  }
 
-> p {
-  color: ${props => props.theme.alto}
+  > p {
+  color: ${props => props.theme.alto};
+  font-size: 13px;
+  font-weight: bold;
 }`;
+
+const ListView = styled.ul`
+  position: absolute;
+  display: table;
+  bottom: 65px;
+  right: 28px;
+  color: ${props => props.theme.alto};
+  border: 1px solid ${props => props.theme.alto};
+  background-color: #121212;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &.hide {
+    display: none;
+  }
+
+  > li {
+    display: table-cell;
+    width: 100%;
+    height: 100%;
+    padding: 5px 45px;
+    &:hover {
+
+    }
+  }
+
+`;
 
 const GetMe = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.getMeReducer.user);
+
+  const expandOptions = () => {
+    const hide = 'hide';
+    document.body.querySelector('i.fa-angle-up').classList.toggle(hide);
+    document.body.querySelector('i.fa-angle-down').classList.toggle(hide);
+    document.body.querySelector('ul.user-list').classList.toggle(hide);
+  }
+
+  const logout = () => {
+    localStorage.removeItem('accessToken');
+    window.location.href = window.location.origin;
+  }
 
   useEffect(() => {
     if (!user.username) dispatch(getMeFetch());
   }, [dispatch, user])
 
   return (
-    <GetMeView>
-      {user.username ?
-        <>
-          <p>{user.username}</p>
-          <img src={user.photo} alt='you' />
-        </>
-        : null}
-    </GetMeView>
+    <>
+      <ListView className='user-list hide'><li onClick={logout}>Logout</li></ListView>
+      <GetMeView onClick={expandOptions}>
+        {user.username ?
+          <>
+            <i className="fas fa-angle-down"></i>
+            <i className="fas fa-angle-up hide"></i>
+            <p>{user.username}</p>
+            <img src={user.photo} alt='you' />
+          </>
+          : null}
+      </GetMeView>
+    </>
   );
 }
 
