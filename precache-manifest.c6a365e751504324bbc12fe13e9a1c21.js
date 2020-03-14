639035c52261f880ec838cc7c3314c0b630ac746
Marcin Lasecki <marcin.lasecki@pearson.com>
2020-03-14 15:26:04 +0100

    Updates

---
+++ precache-manifest.c6a365e751504324bbc12fe13e9a1c21.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac26b9b5c9f0aa000771127ac6c8a6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6722affdd230791296a7",
+    "url": "/static/css/2.20fddfda.chunk.css"
+  },
+  {
+    "revision": "cebd2546b5f19a360700",
+    "url": "/static/css/main.681255b4.chunk.css"
+  },
+  {
+    "revision": "6722affdd230791296a7",
+    "url": "/static/js/2.0710ff0c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.0710ff0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cebd2546b5f19a360700",
+    "url": "/static/js/main.8c150b9e.chunk.js"
+  },
+  {
+    "revision": "db0d2a0cb5a070f08dab",
+    "url": "/static/js/runtime-main.9fb5a95c.js"
+  },
+  {
+    "revision": "4cbc434bf7bddc05063cc9ec37a9750d",
+    "url": "/static/media/music.4cbc434b.jpg"
+  },
+  {
+    "revision": "29cbc2bb7a39ec1f677be9a368fd26d0",
+    "url": "/static/media/yt.29cbc2bb.webp"
+  }
+]);
